ec53cf486fa9e8b705e240a250b1fce3bfdf3342
shirazi97 <shirazi1997@gmail.com>
2023-04-02 17:12:09 +0300

    improvements

--- m_shell.cpp
+++ m_shell.cpp
@@ -23,7 +23,7 @@ int main() {
     char command[1024];
     char *token;
     char *outfile;
-    int i, fd, amper, redirect, retid, status, quit, dollar, echo;
+    int i, fd, amper, redirect, retid, status;
     char *argv[10];
     strcpy(PROMPT_BUFFER, "hello: ");
 
@@ -54,23 +54,54 @@ int main() {
             amper = 0;
 
         if (!strcmp(argv[0], "quit")) {
-            quit = 1;
-        } else {
-            quit = 0;
+            exit(0);
         }
 
         if (argv[0][0] == '$') {
-            dollar = 1;
-        } else {
-            dollar = 0;
+            if ((!strcmp(argv[1], "=")) && (argv[2] != NULL)) {
+                std::string str_var(argv[0]);
+                string sliced_var = str_var.substr(1);
+                if (setenv(sliced_var.c_str(), argv[2], 1) != 0) {
+                    cout << "failed!" << endl;
+                } else {
+                    cout << endl;
+                }
+            } else {
+                cout << "wrong command!" << endl;
+            }
+            continue;
+        }
+
+        if (!strcmp(argv[0], "read")) {
+            char buffer[100];
+            char buffer2[100];
+            strcpy(buffer, argv[1]);
+            scanf("%s", buffer2);
+            setenv(buffer, buffer2, 1);
+            continue;
         }
 
         if (!strcmp(argv[0], "echo")) {
-            echo = 1;
-            cout << "echo pppooo!" << endl;
-        } else {
-            echo = 0;
+            int i = 1; // start from the second argument
+            while (argv[i] != NULL) {
+                if (argv[i][0] == '$') {
+                    char *var_value = getenv(argv[i] + 1);
+                    if (var_value != NULL) {
+                        cout << var_value << " ";
+                    } else {
+                        cout << argv[i] << " ";
+                    }
+                } else {
+                    cout << argv[i] << " ";
+                }
+                i++; // move to the next argument
+            }
+            cout << endl;
+            continue;
         }
+
+
+
 
 
         /* Does command line have prompt = */
@@ -95,10 +126,13 @@ int main() {
         } else
             redirect = 0;
 
-        /* for commands not part of the shell command language */
 
+
+
+        /* for commands not part of the shell command language */
+//-------------------------------------------------------------------------------------------------------------------------------
         if (fork() == 0) {
-            if (quit || control_c) {
+            if (control_c) {
                 exit(0);
             }
             /* redirection of IO ? */
@@ -134,52 +168,14 @@ int main() {
                         break;
                 }
 
-
             }
-            if (dollar) {
-                cout << "ciii" << endl;
-                if ((!strcmp(argv[1], "=")) && (argv[2] != NULL)) {
-                    cout << "cii222i" << endl;
-                    std::string str_var(argv[0]);
-                    string sliced_var = str_var.substr(1);
-                    if (setenv(sliced_var.c_str(), argv[2], 1) != 0) {
-                        cout << "failed!" << endl;
-                    } else {
-                        cout << "setenv!!" << endl;
-                    }
-                } else {
-                    exit(1);
-                }
-                exit(0);
-            }
-
-            if (echo) {
-                cout << "kan11" << endl;
-                for (int i = 1; i < 10; i++) {
-                    cout << "jhfkjwgfkw";
-                    if (argv[i][0] == '$') {
-                        std::string str_var(argv[0]);
-                        string sliced_var = str_var.substr(1);
-                        char *var_value = getenv(sliced_var.c_str());
-                        printf("%s ", var_value);
-                        printf("%s ", "var_value");
-                    } else {
-                        printf("%s ", argv[i]);
-                        printf("%s ", "argv[i]");
-                    }
-
-                }
-            }
-
             execvp(argv[0], argv);
         }
         /* parent continues here */
         if (control_c) {
             control_c = 0;
         }
-        if (quit) {
-            exit(0);
-        }
+
         if (amper == 0)
             retid = wait(&status);
     }
